dfae53ec728995542502c3d6ad34c4f76701d194
Andre Manchanthasouk <dremanchan@gmail.com>
2022-02-06 16:36:02 -0600

    made buttoncheck page and imported gamepad api and hooks.

---
+++ src/components/ButtonCheck/ButtonCheck.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { useGamepads } from 'react-gamepads';
+
+export default function ButtonCheck() {
+  const [gamepads, setGamepads] = useState({});
+  useGamepads(gamepads => setGamepads(gamepads));
+
+  const gamepadDisplay = Object.keys(gamepads).map(gamepadId => {
+    // console.log("displaying gamepad", gamepads[gamepadId]);
+    return (
+      <div>
+        <h2>{gamepads[gamepadId].id}</h2>
+        {gamepads[gamepadId].buttons &&
+          gamepads[gamepadId].buttons.map((button, index) => (
+            <div>
+              {index}: {button.pressed ? 'True' : 'False'}
+            </div>
+          ))}
+      </div>
+    );
+  });
+
+  return (
+    <div className="Gamepads">
+      <h1>Gamepads</h1>
+      {gamepadDisplay}
+    </div>
+  );
+}
