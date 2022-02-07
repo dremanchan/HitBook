1028e121b27fdb4119cc6337f9eca6947dfadea7
Andre Manchanthasouk <dremanchan@gmail.com>
2022-02-07 14:34:36 -0600

    Info page done.

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import './InfoPage.css'
+import { Link, } from 'react-router-dom';
 
 // This is one of our simplest components
 // It doesn't have local state
@@ -6,10 +8,27 @@ import React from 'react';
 // or even care what the redux state is
 
 function InfoPage() {
-  return (
+  return ( <>
+    <h1 className="page-title">How to use this app:</h1>
     <div className="container">
-      <p>Info Page</p>
+      
+
+      <ol className="instructions">
+        <li>Select your game</li>
+        <li>Select your character</li>
+        <li>Star a character to add it to favorites</li>
+        <li>Click the star on the favorites page <br></br>
+          to unfavorite a character</li>
+      </ol>
+
+      
     </div>
+    <div className="buttonDiv">
+    <Link to="game-select">
+        <button className="gameBtn"> Game Select</button>
+      </Link>
+      </div>
+    </>
   );
 }
 
