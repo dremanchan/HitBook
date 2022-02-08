dccf5eeee6b17aef8145f5fe5c32f23950b338ac
Andre Manchanthasouk <dremanchan@gmail.com>
2022-02-08 11:42:33 -0600

    added tables to database.sql

--- database.sql
+++ database.sql
@@ -6,5 +6,21 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "admin" BOOLEAN DEFAULT FALSE,
+);
+
+CREATE TABLE "character" (
+    "id" SERIAL PRIMARY KEY,
+    "gameId" INTEGER,
+    "moveList" VARCHAR (4096),
+    "strategy" VARCHAR (4096),
+    "combos" VARCHAR (2048),
+    "image" VARCHAR (2048)
+);
+
+
+CREATE TABLE "game" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (80),
 );
