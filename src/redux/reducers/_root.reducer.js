df6e216e7b9eb6333d24d1c0fcfe4d87b4a78f98
Andre Manchanthasouk <dremanchan@gmail.com>
2022-02-09 13:07:44 -0600

    created details reducer

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
 import character from './character.reducer';
+import details from './details.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -13,6 +14,7 @@ const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
   character, // character data from db
+  details, // organized character data
 });
 
 export default rootReducer;
