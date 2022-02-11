b6161103fcda615f95c9879eb5c071c741b1c5ac
Andre Manchanthasouk <dremanchan@gmail.com>
2022-02-11 10:01:38 -0600

    created favorites page.

---
+++ src/components/FavoritePage/FavoritePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+function FavoritePage() {
+    return (
+        <>
+            <h1>Favorites</h1>
+
+            {/* Render favorites here with toggleFavorites button */}
+        </>
+    )
+}
+
+export default FavoritePage;
