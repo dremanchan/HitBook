4dd5ed1b36999596fde0caa4744966519e82c627
Andre Manchanthasouk <dremanchan@gmail.com>
2022-02-14 06:56:48 -0600

    created post character route.

--- server/routes/character.router.js
+++ server/routes/character.router.js
@@ -24,8 +24,25 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
-  // POST route code here
+router.post('/', rejectUnauthenticated, (req, res) => {
+  const queryText = `
+  INSERT INTO "character" ( "name", "gameId", "strategy", "combos", "image","thumbnail")
+  VALUES ($1, $2, $3, $4, $5, $6);`;
+  const queryParams = [
+    req.body.name,
+    req.body.gameId,
+    req.body.strategy,
+    req.body.combos,
+    req.body.image,
+    req.body.thumbnail
+  ]
+  pool
+    .query(queryText, queryParams)
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+      console.error('POST character failed', err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
--- src/components/FavoritePage/FavoritePage.jsx
+++ src/components/FavoritePage/FavoritePage.jsx
@@ -1,12 +1,52 @@
-import React from 'react';
+import { React, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
 
 function FavoritePage() {
+    const dispatch = useDispatch();
+    const details = useSelector((store) => store.details);
+    const user = useSelector((store) => store.user);
+    const favorite = useSelector ((store) => store.favorite);
+    const character = useSelector ((store) => store.character);
+
+    const params = useParams();
+    const characterId = params.id;
+    const currentUser = user.id;
+
+    // Renders details on page load
+
+    
+    const nCharacterId = parseInt(characterId);
+    function deleteFavorite() {
+        dispatch({
+          type: "DELETE_FAVORITE",
+          payload: currentUser,
+        });
+      }
+
     return (
-        <>
+        <>  
             <h1>Favorites</h1>
+            <Container>
+
+            
+                <h3>
+                    {details.characterName}
+                    <Button onClick={deleteFavorite}>
+                        Unfavorite
+                    </Button>
+                </h3>
+                <Link to={`/smashcharacter/${character.id}`}>
+                    <img src={details.characterImg} />
+                </Link>
+           
+            
+
 
             {/* Render favorites here with toggleFavorites button */}
+            </Container>
         </>
     )
 }
