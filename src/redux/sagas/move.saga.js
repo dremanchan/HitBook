784a50fa4e65810a7ac371e7899db54093e3be7e
Andre Manchanthasouk <dremanchan@gmail.com>
2022-02-17 05:21:36 -0600

    add move saga created.

--- src/redux/sagas/move.saga.js
+++ src/redux/sagas/move.saga.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { put, takeEvery } from 'redux-saga/effects';
-
+// GET moves
 function* fetchMoves(action) {
     try {
         const move = yield axios.get('/api/move', {params: {id: action.payload}});
@@ -10,6 +10,14 @@ function* fetchMoves(action) {
     catch (err) {
         console.error('fetchMoves failed', err);
     }
+}
+
+// Post Moves
+function* addMove(action) {
+    yield axios.post('/api/move', action.payload);
+
+    yield put ({ type: 'FETCH_MOVES'})
+
 }
 
 // Delete moves saga
@@ -26,7 +34,9 @@ function* deleteMove(action) {
 
 function* moveSaga() {
     yield takeEvery('FETCH_MOVES', fetchMoves);
+    yield takeEvery('ADD_MOVE', addMove);
     yield takeEvery('DELETE_SET_MOVE', deleteMove);
+
 }
 
 export default moveSaga;
